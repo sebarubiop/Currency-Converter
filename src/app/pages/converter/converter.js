19ad5a00807aab4458dd7a4c15718f741a257b9e
sebarubiop <sebarubiop@gmail.com>
2017-04-05 14:23:53 +1000

    finishing currency converter

--- src/app/pages/converter/converter.js
+++ src/app/pages/converter/converter.js
@@ -9,61 +9,42 @@
   function converterCtrl($scope,$http,$timeout) {
     
     var api_url = 'https://api.fixer.io/';  
-    // console.log($stateParams);
-    // $scope.request = $stateParams;
     
-    // $scope.amount1 = 1;
+    $scope.amount1 = 1;
     $scope.currency1 = 'AUD';
     $scope.currency2 = 'USD';
 
+
+    var calculate = function(am1,cu1,cu2){
+         $http({
+        url: api_url+'latest', 
+        method: "GET",
+        params:{base:cu1,symbols:cu2}
+        })
+        .then(function(response) {
+            $scope.rate = Object.values(response.data.rates)[0];  
+            $scope.amount2 = $scope.rate*am1;
+            
+        });
+    }
+
+   calculate($scope.amount1,$scope.currency1,$scope.currency2);
+
     $scope.convertAmount1 = function(am1,cu1,cu2){
-        console.log('$scope.currency1',cu1)
-        console.log('$scope.currency2',cu2)
         
-        $http({
-            url: api_url+'latest', 
-            method: "GET",
-            params:{base:cu1,symbols:cu2}
-            })
-            .then(function(response) {
-                 $timeout(function() {
-                var rate = Object.values(response.data.rates)[0];  
-                console.log('rate',rate)               
-                $scope.amount2 = rate*am1;
-                console.log('amount2',$scope.amount2)
-                 },0);
-            });
+        calculate(am1,cu1,cu2);
     }
 
-    $scope.convertAmount2 = function(am2){
-        // console.log('am2',am2)
+    $scope.convertCurrency1 = function(am1,cu1,cu2){
+        calculate(am1,cu1,cu2);
         
-        $http({
-            url: api_url+'latest', 
-            method: "GET",
-            params:{base:$scope.currency2,symbols:$scope.currency1}
-            })
-            .then(function(response) {
-                 $timeout(function() {
-                var rate = Object.values(response.data.rates)[0];    
-                console.log('rate',rate)            
-                $scope.amount1 = rate*am2;
-                console.log('amount1',$scope.amount1)
-                 },0);
-            });
     }
 
+    $scope.convertCurrency2 = function(am1,cu1,cu2){
+        calculate(am1,cu1,cu2);
+        
+    }
 
-  //Latest//////////////////////////////////////////////////////////////////////////////////////////////////////
-   $http({
-      url: api_url+'latest', 
-      method: "GET",
-      params:{base:'AUD',symbols:'USD'}
-    })
-    .then(function(response) {
-        $scope.convertion = response.data;
-        console.log('convertion',$scope.convertion)
-    });
 
     //Historical rates///////////////////////////////////////////////////////////////////////////////////////////
    $scope.keys;
@@ -98,10 +79,6 @@
         
         });
     });
-
-
-    
-
     
   }
 })();
